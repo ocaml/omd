08acff4e326fe8d6258dc0e5786f573ad15be925
Thomas Gazagnaire <thomas@gazagnaire.org>
2013-01-27 18:02:23 +0000

    Continue to refactor the COW API. Still half-broken though ...

--- test.ml
+++ test.ml
@@ -1,8 +1,8 @@
-let () =
-  if Array.length Sys.argv < 1 then (
-    Printf.eprintf "usage: %s <dirname>\n" (Filename.basename Sys.argv.(0));
-    exit 1;
-  )
+let dir =
+  if Array.length Sys.argv < 1 then
+    Sys.getcwd ()
+  else
+    Sys.argv.(1)
 
 let process file =
   let html = (Filename.chop_extension file) ^ ".html" in
@@ -10,12 +10,11 @@ let process file =
     Printf.eprintf "File %s does not exist.\n" html;
     exit 2;
   );
-  let t1 = Md.of_file html in
-  let t2 = Md.of_string (Md.to_string t1) in
-  Md.assert_equal t1 t2
+  let t1 = Cow.Md.of_file html in
+  let t2 = Cow.Md.of_string (Cow.Md.to_string t1) in
+  Cow.Md.assert_equal t1 t2
 
 let () =
-  let dir = Sys.argv.(1) in
   let files = Sys.readdir dir in
   let files = Array.to_list files in
   let files = List.map (fun f -> Filename.concat dir f) files in
