252a4224362b1e9ff4627218056f6ebc1cb877a4
Philippe Wang <philippe.wang@gmail.com>
2013-07-25 16:30:47 +0100

    md.ml: the file that (now) contains the type definition of markdown tree and the implementation of html_of_md: md -> string

--- src/md.ml
+++ src/md.ml
@@ -19,4 +19,59 @@ type md_element =
   | Html of string
 and li = Li of md
 and md = md_element list
-    
+
+let htmlentities s = s
+
+let html_of_md md = 
+  let b = Buffer.create 42 in
+  let rec loop = function
+    | Paragraph md ->
+        Buffer.add_string b "<p>";
+        List.iter loop md;
+        Buffer.add_string b "</p>"
+    | Text t ->
+        Buffer.add_string b t
+    | Emph md ->
+        Buffer.add_string b "<em>";
+        List.iter loop md;
+        Buffer.add_string b "</em>"
+    | Bold md ->
+        Buffer.add_string b "<strong>";
+        List.iter loop md;
+        Buffer.add_string b "</strong>"
+    | Ul l ->
+        Buffer.add_string b "<ul>";
+        List.iter
+          (fun (Li li) ->
+             Buffer.add_string b "<li>";
+             List.iter loop li;
+             Buffer.add_string b "</li>")
+          l;
+          Buffer.add_string b "</ul>"
+    | Ol l ->
+        Buffer.add_string b "<ol>";
+        List.iter
+          (fun (Li li) ->
+             Buffer.add_string b "<li>";
+             List.iter loop li;
+             Buffer.add_string b "</li>")
+          l;
+          Buffer.add_string b "</ol>"
+    | Sp n ->
+        for i = 0 to n do
+          Buffer.add_char b ' '
+        done
+    | Code c ->
+        Buffer.add_string b "<pre>";
+        Buffer.add_string b (htmlentities c);
+        Buffer.add_string b "</pre>"
+    | Br ->
+        Buffer.add_string b "<br/>"
+    | Hr ->
+        Buffer.add_string b "<hr/>"
+    | Html s ->
+        Buffer.add_string b s
+  in 
+    List.iter loop md;
+    Buffer.contents b
+
