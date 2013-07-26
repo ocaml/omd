cc6ddae37681920bf633aabcf30cac51161baa14
Philippe Wang <philippe.wang@gmail.com>
2013-07-26 20:29:57 +0100

    md.ml: titles

--- src/md.ml
+++ src/md.ml
@@ -12,15 +12,35 @@ type md_element =
   | Bold of md
   | Ul of li list
   | Ol of li list
-  | Sp of int (* spaces *)
   | Code of string (* html entities are to be converted *later* *)
   | Br
   | Hr
+  | Url of string
   | Html of string
+  | H1 of md
+  | H2 of md
+  | H3 of md
+  | H4 of md
+  | H5 of md
+  | H6 of md
+  | NL
 and li = Li of md
 and md = md_element list
 
-let htmlentities s = s
+let htmlentities s =
+  let b = Buffer.create 42 in
+    for i = 0 to String.length s - 1 do
+      match s.[i] with
+        | ( '0' .. '9' | 'a' .. 'z' | 'A' .. 'Z' ) as c -> Buffer.add_char b c
+        | '"' -> Buffer.add_string b "&quot;"
+        | '\'' -> Buffer.add_string b "&apos;"
+        | '&' -> Buffer.add_string b "&amp;"
+        | '<' -> Buffer.add_string b "&lt;"
+        | '>' -> Buffer.add_string b "&gt;"
+        | '\\' -> Buffer.add_string b "&#92;"
+        | c -> Buffer.add_char b c
+    done;
+    Buffer.contents b
 
 let html_of_md md = 
   let b = Buffer.create 42 in
@@ -43,24 +63,20 @@ let html_of_md md =
         Buffer.add_string b "<ul>";
         List.iter
           (fun (Li li) ->
-             Buffer.add_string b "<li>";
-             List.iter loop li;
-             Buffer.add_string b "</li>")
+            Buffer.add_string b "<li>";
+            List.iter loop li;
+            Buffer.add_string b "</li>")
           l;
-          Buffer.add_string b "</ul>"
+        Buffer.add_string b "</ul>"
     | Ol l ->
         Buffer.add_string b "<ol>";
         List.iter
           (fun (Li li) ->
-             Buffer.add_string b "<li>";
-             List.iter loop li;
-             Buffer.add_string b "</li>")
+            Buffer.add_string b "<li>";
+            List.iter loop li;
+            Buffer.add_string b "</li>")
           l;
-          Buffer.add_string b "</ol>"
-    | Sp n ->
-        for i = 0 to n do
-          Buffer.add_char b ' '
-        done
+        Buffer.add_string b "</ol>"
     | Code c ->
         Buffer.add_string b "<pre>";
         Buffer.add_string b (htmlentities c);
@@ -71,6 +87,39 @@ let html_of_md md =
         Buffer.add_string b "<hr/>"
     | Html s ->
         Buffer.add_string b s
+    | Url s ->
+        let s = htmlentities s in
+          Buffer.add_string b "<a href='";
+          Buffer.add_string b s;
+          Buffer.add_string b "'>";
+          Buffer.add_string b s;
+          Buffer.add_string b "</a>";
+    | H1 md ->
+        Buffer.add_string b "<h1>";
+        List.iter loop md;
+        Buffer.add_string b "</h1>"
+    | H2 md ->
+        Buffer.add_string b "<h2>";
+        List.iter loop md;
+        Buffer.add_string b "</h2>"
+    | H3 md ->
+        Buffer.add_string b "<h3>";
+        List.iter loop md;
+        Buffer.add_string b "</h3>"
+    | H4 md ->
+        Buffer.add_string b "<h4>";
+        List.iter loop md;
+        Buffer.add_string b "</h4>"
+    | H5 md ->
+        Buffer.add_string b "<h5>";
+        List.iter loop md;
+        Buffer.add_string b "</h5>"
+    | H6 md ->
+        Buffer.add_string b "<h6>";
+        List.iter loop md;
+        Buffer.add_string b "</h6>"
+    | NL ->
+        Buffer.add_char b '\n'
   in 
     List.iter loop md;
     Buffer.contents b
