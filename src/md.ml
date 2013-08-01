5d3dd12670e09fa0dc426486546c31bd5e8404f0
Philippe Wang <philippe.wang@gmail.com>
2013-08-01 03:39:10 +0100

    md.ml: lists cosmetics

--- src/md.ml
+++ src/md.ml
@@ -5,6 +5,9 @@
 (* http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html         *)
 (***********************************************************************)
 
+let pindent = false
+let pindent = true
+
 type md_element = 
   | Paragraph of md
   | Text of string
@@ -44,94 +47,6 @@ let htmlentities s =
     done;
     Buffer.contents b
 
-(* let html_of_md md = *)
-(*   let b = Buffer.create 42 in *)
-(*   let rec loop = function *)
-(*     | Paragraph md -> *)
-(*         Buffer.add_string b "<p>"; *)
-(*         List.iter loop md; *)
-(*         Buffer.add_string b "</p>" *)
-(*     | Text t -> *)
-(*         Buffer.add_string b t *)
-(*     | Emph md -> *)
-(*         Buffer.add_string b "<em>"; *)
-(*         List.iter loop md; *)
-(*         Buffer.add_string b "</em>" *)
-(*     | Bold md -> *)
-(*         Buffer.add_string b "<strong>"; *)
-(*         List.iter loop md; *)
-(*         Buffer.add_string b "</strong>" *)
-(*     | Ul l -> *)
-(*         Buffer.add_string b "<ul>"; *)
-(*         List.iter *)
-(*           (fun (Li li) -> *)
-(*             Buffer.add_string b "<li>"; *)
-(*             List.iter loop li; *)
-(*             Buffer.add_string b "</li>") *)
-(*           l; *)
-(*         Buffer.add_string b "</ul>" *)
-(*     | Ol l -> *)
-(*         Buffer.add_string b "<ol>"; *)
-(*         List.iter *)
-(*           (fun (Li li) -> *)
-(*             Buffer.add_string b "<li>"; *)
-(*             List.iter loop li; *)
-(*             Buffer.add_string b "</li>") *)
-(*           l; *)
-(*         Buffer.add_string b "</ol>" *)
-(*     | Code c -> *)
-(*         Buffer.add_string b "<pre>"; *)
-(*         Buffer.add_string b (htmlentities c); *)
-(*         Buffer.add_string b "</pre>" *)
-(*     | Br -> *)
-(*         Buffer.add_string b "<br/>" *)
-(*     | Hr -> *)
-(*         Buffer.add_string b "<hr/>" *)
-(*     | Html s -> *)
-(*         Buffer.add_string b s *)
-(*     | Url (href,s,title) -> *)
-(*         let s = htmlentities s in *)
-(*           Buffer.add_string b "<a href='"; *)
-(*           Buffer.add_string b (htmlentities href); *)
-(*           Buffer.add_string b "'"; *)
-(*           if title <> "" then *)
-(*             begin *)
-(*               Buffer.add_string b " title='"; *)
-(*               Buffer.add_string b (htmlentities title); *)
-(*               Buffer.add_string b "'"; *)
-(*             end; *)
-(*           Buffer.add_string b ">"; *)
-(*           Buffer.add_string b s; *)
-(*           Buffer.add_string b "</a>"; *)
-(*     | H1 md -> *)
-(*         Buffer.add_string b "<h1>"; *)
-(*         List.iter loop md; *)
-(*         Buffer.add_string b "</h1>" *)
-(*     | H2 md -> *)
-(*         Buffer.add_string b "<h2>"; *)
-(*         List.iter loop md; *)
-(*         Buffer.add_string b "</h2>" *)
-(*     | H3 md -> *)
-(*         Buffer.add_string b "<h3>"; *)
-(*         List.iter loop md; *)
-(*         Buffer.add_string b "</h3>" *)
-(*     | H4 md -> *)
-(*         Buffer.add_string b "<h4>"; *)
-(*         List.iter loop md; *)
-(*         Buffer.add_string b "</h4>" *)
-(*     | H5 md -> *)
-(*         Buffer.add_string b "<h5>"; *)
-(*         List.iter loop md; *)
-(*         Buffer.add_string b "</h5>" *)
-(*     | H6 md -> *)
-(*         Buffer.add_string b "<h6>"; *)
-(*         List.iter loop md; *)
-(*         Buffer.add_string b "</h6>" *)
-(*     | NL -> *)
-(*         Buffer.add_char b '\n' *)
-(*   in *)
-(*     List.iter loop md; *)
-(*     Buffer.contents b *)
 
 let make_paragraphs md =
   let rec remove_prefix prefix = function
@@ -215,30 +130,38 @@ let rec html_of_md md =
         Buffer.add_string b "</strong>";
         loop indent tl
     | Ul l :: tl ->
-        Buffer.add_string b "\n";
-        for i = 0 to indent do Buffer.add_char b ' ' done;
-        Buffer.add_string b "<ul>\n";
+        if pindent then Buffer.add_char b '\n';
+        if pindent then for i = 0 to indent do Buffer.add_char b ' ' done;
+        Buffer.add_string b "<ul>";
+        if pindent then Buffer.add_char b '\n';
         List.iter
           (fun (Li li) ->
-            for i = 0 to indent + 1 do Buffer.add_char b ' ' done;
+            if pindent then for i = 0 to indent + 1 do Buffer.add_char b ' ' done;
             Buffer.add_string b "<li>";
             loop (indent+2) li;
-            Buffer.add_string b "\n";
-            for i = 0 to indent + 1 do Buffer.add_char b ' ' done;
-            Buffer.add_string b "</li>\n")
+            Buffer.add_string b "</li>";
+            if pindent then Buffer.add_char b '\n')
           l;
-        for i = 0 to indent do Buffer.add_char b ' ' done;
-        Buffer.add_string b "</ul>\n";
+        if pindent then for i = 0 to indent do Buffer.add_char b ' ' done;
+        Buffer.add_string b "</ul>";
+        if pindent then Buffer.add_char b '\n';
         loop indent tl
     | Ol l :: tl ->
+        if pindent then Buffer.add_char b '\n';
+        if pindent then for i = 0 to indent do Buffer.add_char b ' ' done;
         Buffer.add_string b "<ol>";
+        if pindent then Buffer.add_char b '\n';
         List.iter
           (fun (Li li) ->
+            if pindent then for i = 0 to indent + 1 do Buffer.add_char b ' ' done;
             Buffer.add_string b "<li>";
-            loop indent li;
-            Buffer.add_string b "</li>")
+            loop (indent+2) li;
+            Buffer.add_string b "</li>";
+            if pindent then Buffer.add_char b '\n')
           l;
-        Buffer.add_string b "</ol>\n";
+        if pindent then for i = 0 to indent do Buffer.add_char b ' ' done;
+        Buffer.add_string b "</ol>";
+        if pindent then Buffer.add_char b '\n';
         loop indent tl
     | Code c :: tl ->
         Buffer.add_string b "<pre>";
