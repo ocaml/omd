558c189c0c45c18e588b75f0019399ad25f589c3
Philippe Wang <philippe.wang@gmail.com>
2013-08-07 15:32:26 +0100

    md_lib.ml: library interface for omd

---
+++ src/md_lib.ml
@@ -0,0 +1,77 @@
+(***********************************************************************)
+(* omd: Markdown frontend in OCaml                                     *)
+(* (c) 2013 by Philippe Wang <philippe.wang@cl.cam.ac.uk>              *)
+(* Licence : CeCILL-B                                                  *)
+(* http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html         *)
+(***********************************************************************)
+
+module Md : sig
+  (** A element of a Markdown document *)
+  type md_element = 
+    | Paragraph of md
+    | Text of string
+    | Emph of md
+    | Bold of md
+    | Ul of li list
+    | Ol of li list
+    | Code of string (* html entities are to be converted *later* *)
+    | Br
+    | Hr
+    | Url of href * string * title
+    | Html of string
+    | H1 of md
+    | H2 of md
+    | H3 of md
+    | H4 of md
+    | H5 of md
+    | H6 of md
+    | Img of alt * src * title
+    | NL
+
+  (** html img tag attribute *)
+  and alt = string
+
+  (** html attribute *)
+  and src = string
+
+  (** html attribute *)
+  and href = string
+
+  (** html attribute *)
+  and title = string
+
+  (** element of a list *)
+  and li = Li of md
+
+  (** Representation of a Markdown document.  *) 
+  and md = md_element list
+
+  (** Abstract representation of the lexer's tokens *)
+  type token
+
+  (** Translate a raw string into tokens for the parser *)
+  val lex : string -> token list
+
+  (** Translate tokens to Markdown representation *)
+  val parse : token list -> md
+end =
+struct
+  include Md
+
+  include Md_lexer
+    
+  include Md_parser
+
+  type token = tag Md_lexer.t
+
+  let lex : string -> token list = Md_lexer.lex
+
+  let parse : token list -> md = Md_parser.parse
+
+  let html_of_string (html:string) : string =
+    Md.html_of_md (parse (lex html))
+
+end
+
+
+
