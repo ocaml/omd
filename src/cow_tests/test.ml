66d97fcd04db50b828c0004cf17a97842a722fc8
Jeremy Yallop <yallop@gmail.com>
2013-08-08 21:31:55 +0100

    Adapt test.ml to omd.

--- src/cow_tests/test.ml
+++ src/cow_tests/test.ml
@@ -2,7 +2,16 @@ let dir =
   if Array.length Sys.argv > 1 then
     Sys.argv.(1)
   else
-    Filename.concat (Sys.getcwd ()) "lib_tests/md"
+    Filename.concat (Sys.getcwd ()) "cow_tests"
+
+let slurp filename =
+  let file = open_in filename in
+  let size = in_channel_length file in
+  let buf = String.create size in
+  begin
+    really_input file buf 0 size;
+    buf
+  end
 
 let process file =
   let html = (Filename.chop_extension file) ^ ".html" in
@@ -10,11 +19,15 @@ let process file =
     Printf.eprintf "File %s does not exist.\n" html;
     exit 2;
   );
-  let t1 = Cow.Md.of_file html in
-  let t2 = Cow.Md.of_string (Cow.Md.to_string t1) in
-  Cow.Md.assert_equal t1 t2
+  let expected = slurp html in
+  let observed = Md_lib.Md.(html_of_md (parse (lex (slurp file)))) in
+  if expected <> observed then
+    Printf.fprintf stderr "FAILURE: %s\n" file
+  else
+    Printf.fprintf stderr "SUCCESS: %s\n" file
 
 let () =
+  let () = prerr_endline ("reading directory " ^ dir) in
   let files = Sys.readdir dir in
   let files = Array.to_list files in
   let files = List.map (fun f -> Filename.concat dir f) files in
