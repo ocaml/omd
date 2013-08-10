3a334fcfb88edf27ec85a58958a54ecb2844a804
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2013-08-10 21:53:04 +0200

    Build using OASIS.

---
+++ setup.ml
@@ -0,0 +1,11 @@
+let () =
+  try Topdirs.dir_directory (Sys.getenv "OCAML_TOPLEVEL_PATH")
+  with Not_found -> ();;
+
+(* OASIS_START *)
+(* DO NOT EDIT (digest: 7f47a529f70709161149c201ccd90f0b) *)
+#use "topfind";;
+#require "oasis.dynrun";;
+open OASISDynRun;;
+(* OASIS_STOP *)
+let () = setup ();;
