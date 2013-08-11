6ba59772f6d89ef6b332ad1b4ee7035942469c91
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2013-08-11 14:23:54 +0200

    Enable tests (compilation + oasis + make).

--- tests/cow/test.ml
+++ tests/cow/test.ml
@@ -2,7 +2,7 @@ let dir =
   if Array.length Sys.argv > 1 then
     Sys.argv.(1)
   else
-    Filename.concat (Sys.getcwd ()) "cow_tests"
+    Filename.concat (Sys.getcwd ()) "tests/cow"
 
 let slurp filename =
   let file = open_in filename in
@@ -20,16 +20,15 @@ let process file =
     exit 2;
   );
   let expected = slurp html in
-  let observed = Md_lib.Md.(html_of_md (parse (lex (slurp file)))) in
+  let observed = Omd.(to_html (parse (lex (slurp file)))) in
   if expected <> observed then
     Printf.fprintf stderr "FAILURE: %s\n" file
   else
     Printf.fprintf stderr "SUCCESS: %s\n" file
 
 let () =
-  let () = prerr_endline ("reading directory " ^ dir) in
-  let files = Sys.readdir dir in
-  let files = Array.to_list files in
+  prerr_endline ("Reading directory " ^ dir);
+  let files = Array.to_list (Sys.readdir dir) in
   let files = List.map (fun f -> Filename.concat dir f) files in
   let md_files = List.filter (fun f -> Filename.check_suffix f ".md") files in
   List.iter process md_files
