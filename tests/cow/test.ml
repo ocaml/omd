76d20a2097ee865478699141f38952350b4e2e94
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2013-08-11 14:29:40 +0200

    Report the number of tests that passed and failed.

--- tests/cow/test.ml
+++ tests/cow/test.ml
@@ -1,3 +1,5 @@
+open Printf
+
 let dir =
   if Array.length Sys.argv > 1 then
     Sys.argv.(1)
@@ -13,7 +15,7 @@ let slurp filename =
     buf
   end
 
-let process file =
+let process successes failures file =
   let html = (Filename.chop_extension file) ^ ".html" in
   if not (Sys.file_exists html) then (
     Printf.eprintf "File %s does not exist.\n" html;
@@ -21,15 +23,21 @@ let process file =
   );
   let expected = slurp html in
   let observed = Omd.(to_html (parse (lex (slurp file)))) in
-  if expected <> observed then
-    Printf.fprintf stderr "FAILURE: %s\n" file
-  else
-    Printf.fprintf stderr "SUCCESS: %s\n" file
+  if expected = observed then (
+    eprintf "SUCCESS: %s\n" file;
+    incr successes
+  )
+  else (
+    eprintf "FAILURE: %s\n" file;
+    incr failures
+  )
 
 let () =
   prerr_endline ("Reading directory " ^ dir);
   let files = Array.to_list (Sys.readdir dir) in
   let files = List.map (fun f -> Filename.concat dir f) files in
   let md_files = List.filter (fun f -> Filename.check_suffix f ".md") files in
-  List.iter process md_files
-
+  let successes = ref 0
+  and failures = ref 0 in
+  List.iter (process successes failures) md_files;
+  eprintf "%i test passed;  %i tests failed.\n" !successes !failures
