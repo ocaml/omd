4ccada16d5ccf76cb3113b812f6bcf92bc66db2f
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2013-11-25 00:45:54 +0100

    Do not compare ref. objects for the tests

--- tests/cow/test.ml
+++ tests/cow/test.ml
@@ -1,5 +1,38 @@
 open Printf
 
+(* Compare the structures with the exception of the objects references
+   may contain. *)
+let rec equal m1 m2 =
+  try List.for_all2 equal_el m1 m2
+  with Invalid_argument _ -> false (* different lengths *)
+
+and equal_el md1 md2 =
+  let open Omd in
+  match md1, md2 with
+  | H1 m1, H1 m2 | H2 m1, H2 m2 | H3 m1, H3 m2 | H4 m1, H4 m2 | H5 m1, H5 m2
+  | H6 m1, H6 m2 | Paragraph m1, Paragraph m2 | Emph m1, Emph m2
+  | Bold m1, Bold m2 ->
+     equal m1 m2
+  | Text s1, Text s2 -> s1 = s2
+  | Ul m1, Ul m2 | Ol m1, Ol m2 | Ulp m1, Ulp m2 | Olp m1, Olp m2 ->
+     (try List.for_all2 equal m1 m2
+      with Invalid_argument _ -> false)
+  | Code(n1, c1), Code(n2, c2) | Code_block(n1, c1), Code_block(n2, c2) ->
+     n1 = n2 && c1 = c2
+  | Br, Br | Hr, Hr | NL, NL -> true
+  | Url(h1, m1, t1), Url(h2, m2, t2) ->
+     h1 = h2 && equal m1 m2 && t1 = t2
+  | Ref(_, n1, s1, _), Ref(_, n2, s2, _) -> n1 = n2 && s1 = s2
+  | Img_ref(_, n1, a1, _), Img_ref(_, n2, a2, _) -> n1 = n2 && a1 = a2
+  | Html s1, Html s2 | Html_block s1, Html_block s2
+  | Html_comment s1, Html_comment s2 ->
+     s1 = s2
+  | Blockquote m1, Blockquote m2 -> equal m1 m2
+  | Img(a1, s1, t1), Img(a2, s2, t2) -> a1 = a2 && s1 = s2 && t1 = t2
+  | X x1, X x2 -> x1#name = x2#name
+  | _, _ -> false
+
+
 let dir =
   if Array.length Sys.argv > 1 then
     Sys.argv.(1)
@@ -45,7 +78,7 @@ let process successes failures file =
     eprintf "  observed = %S\n" (observed);
     incr failures
   )
-  else if md <> round_trip then (
+  else if not(equal md round_trip) then (
     eprintf "FAILURE: %s\n" file;
     eprintf "  Omd.of_string(Omd.to_markdown md) <> md\n";
     incr failures
