c71983a2ab90932d614ced0f711c7687dc6f594e
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2018-11-12 14:19:49 +0100

    Cleanup

--- src/html.ml
+++ src/html.ml
@@ -1,5 +1,63 @@
 open Ast
 
+let _id_of_string ids s =
+  let n = String.length s in
+  let out = Buffer.create 0 in
+  (* Put [s] into [b], replacing non-alphanumeric characters with dashes. *)
+  let rec loop started i =
+    if i = n then ()
+    else begin
+      match s.[i] with
+      | 'a' .. 'z' | 'A' .. 'Z' | '0' .. '9' as c ->
+          Buffer.add_char out c ;
+          loop true (i + 1)
+      (* Don't want to start with dashes. *)
+      | _ when not started ->
+          loop false (i + 1)
+      | _ ->
+          Buffer.add_char out '-' ;
+          loop false (i + 1)
+    end
+  in
+  loop false 0 ;
+  let s' = Buffer.contents out in
+  if s' = "" then ""
+  else
+    (* Find out the index of the last character in [s'] that isn't a dash. *)
+    let last_trailing =
+      let rec loop i =
+        if i < 0 || s'.[i] <> '-' then i
+        else loop (i - 1)
+      in
+      loop (String.length s' - 1)
+    in
+    (* Trim trailing dashes. *)
+    ids#mangle @@ String.sub s' 0 (last_trailing + 1)
+
+(* only convert when "necessary" *)
+let htmlentities s =
+  let b = Buffer.create (String.length s) in
+  let rec loop i =
+    if i >= String.length s then
+      Buffer.contents b
+    else begin
+      begin match s.[i] with
+      | '"' ->
+          Buffer.add_string b "&quot;"
+      | '&' ->
+          Buffer.add_string b "&amp;"
+      | '<' ->
+          Buffer.add_string b "&lt;"
+      | '>' ->
+          Buffer.add_string b "&gt;"
+      | c ->
+          Buffer.add_char b c
+      end;
+      loop (succ i)
+    end
+  in
+  loop 0
+
 let percent_encode s =
   let b = Buffer.create (String.length s) in
   String.iter (function
@@ -24,7 +82,7 @@ let rec html_of_md b md =
     | Concat l ->
         List.iter loop l
     | Text t ->
-        Buffer.add_string b (Utils.htmlentities t)
+        Buffer.add_string b (htmlentities t)
     | Emph (Normal, _, md) ->
         Buffer.add_string b "<em>";
         loop md;
@@ -35,7 +93,7 @@ let rec html_of_md b md =
         Buffer.add_string b "</strong>"
     | Code c ->
         Buffer.add_string b "<code>";
-        Buffer.add_string b (Utils.htmlentities c);
+        Buffer.add_string b (htmlentities c);
         Buffer.add_string b "</code>"
     | Hard_break ->
         Buffer.add_string b "<br />\n"
@@ -49,7 +107,7 @@ let rec html_of_md b md =
         | None -> ()
         | Some title ->
             Buffer.add_string b " title=\"";
-            Buffer.add_string b (Utils.htmlentities title);
+            Buffer.add_string b (htmlentities title);
             Buffer.add_string b "\""
         end;
         Buffer.add_string b ">";
@@ -59,13 +117,13 @@ let rec html_of_md b md =
         Buffer.add_string b "<img src=\"";
         Buffer.add_string b (percent_encode destination);
         Buffer.add_string b "\" alt=\"";
-        Buffer.add_string b (Utils.htmlentities (text_of_inline label));
+        Buffer.add_string b (htmlentities (text_of_inline label));
         Buffer.add_string b "\"";
         begin match title with
         | None -> ()
         | Some title ->
             Buffer.add_string b " title=\"";
-            Buffer.add_string b (Utils.htmlentities title);
+            Buffer.add_string b (htmlentities title);
             Buffer.add_string b "\""
         end;
         Buffer.add_string b " />"
@@ -104,11 +162,11 @@ let to_html b md =
         Buffer.add_string b (Printf.sprintf "<pre><code class=\"language-%s\"></code></pre>" info)
     | Code_block ((None | Some (_, "")), Some c) ->
         Buffer.add_string b "<pre><code>";
-        Buffer.add_string b (Utils.htmlentities c);
+        Buffer.add_string b (htmlentities c);
         Buffer.add_string b "\n</code></pre>"
     | Code_block (Some (_, info), Some c) ->
         Printf.bprintf b "<pre><code class=\"language-%s\">" info;
-        Buffer.add_string b (Utils.htmlentities c);
+        Buffer.add_string b (htmlentities c);
         Buffer.add_string b "\n</code></pre>"
     | Thematic_break ->
         Buffer.add_string b "<hr />"
