c09f39558f1ae807c74bc3e4b11dda9047bd5a63
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2018-11-12 13:57:01 +0100

    Cleanup

--- src/omd.mli
+++ src/omd.mli
@@ -59,27 +59,6 @@ val of_channel: in_channel -> t
 
 val of_string: string -> t
 
-val to_html: (* ?pindent:bool -> ?nl2br:bool -> *) t -> string
-(** Translate markdown representation into raw HTML. *)
+val to_html: t -> string
 
 val to_sexp: t -> string
-
-(* val to_markdown: t -> string *)
-(** Translate markdown representation into textual markdown. *)
-
-(* val to_text: t -> string *)
-(** Translate markdown representation into raw text. *)
-
-(* val toc: ?start:int list -> ?depth:int -> t -> t *)
-(** [toc md] returns [toc] a table of contents for [md].
-
-    @param start gives the section for which the TOC must be built.
-    For example [~start:[2;3]] will build the TOC for subsections of
-    the second [H1] header, and within that section, the third [h2]
-    header.  If a number is [0], it means to look for the first
-    section at that level but stop if one encounters any other
-    subsection.  If no subsection exists, an empty TOC [[]] will be
-    returned.  Default: [[]] i.e. list all sections, starting with the
-    first [H1].
-
-    @param depth the table of contents.  Default: [2].  *)
