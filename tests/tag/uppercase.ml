9623b3d73eaea8558a10abbc8d3fb22c5d8f71b2
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2020-06-20 01:09:16 +0200

    Introduce an intermediate representation for HTML

--- tests/tag/uppercase.ml
+++ tests/tag/uppercase.ml
@@ -32,37 +32,37 @@ let main () =
   in
   Arg.parse (Arg.align spec) (fun s -> input := s :: !input) "omd [options] [inputfile1 .. inputfileN] [options]";
   let output = if !output = "" then stdout else open_out_bin !output in
-  let printer =
-    let text p b t =
-      let t = String.uppercase_ascii t in
-      Omd.default_printer.text p b t
-    in
-    let code p b (c: Omd.Inline.code) =
-      let c = {c with content = String.uppercase_ascii c.content} in
-      Omd.default_printer.code p b c
-    in
-    let tag (p: Omd.printer) b (t: Omd.Inline.tag) =
-      match t.tag with
-      | "capitalize" ->
-        p.inline {p with text; code} b t.content
-      | _ -> Omd.default_printer.tag p b t
-    in
-    let tag_block (p: Omd.printer) b t =
-      match t.Omd.Block.tag with
-      | "capitalize" ->
-        let f i block =
-          p.block {p with text; code} b block;
-          if i < List.length t.content - 1 then
-            Buffer.add_char b '\n'
-        in
-        List.iteri f t.content
-      | _ -> Omd.default_printer.tag_block p b t
-    in
-    {Omd.default_printer with tag; tag_block}
-  in
+  (* let printer = *)
+  (*   let text p b t = *)
+  (*     let t = String.uppercase_ascii t in *)
+  (*     Omd.default_printer.text p b t *)
+  (*   in *)
+  (*   let code p b (c: Omd.Inline.code) = *)
+  (*     let c = {c with content = String.uppercase_ascii c.content} in *)
+  (*     Omd.default_printer.code p b c *)
+  (*   in *)
+  (*   let tag (p: Omd.printer) b (t: Omd.Inline.tag) = *)
+  (*     match t.tag with *)
+  (*     | "capitalize" -> *)
+  (*       p.inline {p with text; code} b t.content *)
+  (*     | _ -> Omd.default_printer.tag p b t *)
+  (*   in *)
+  (*   let tag_block (p: Omd.printer) b t = *)
+  (*     match t.Omd.Block.tag with *)
+  (*     | "capitalize" -> *)
+  (*       let f i block = *)
+  (*         p.block {p with text; code} b block; *)
+  (*         if i < List.length t.content - 1 then *)
+  (*           Buffer.add_char b '\n' *)
+  (*       in *)
+  (*       List.iteri f t.content *)
+  (*     | _ -> Omd.default_printer.tag_block p b t *)
+  (*   in *)
+  (*   {Omd.default_printer with tag; tag_block} *)
+  (* in *)
   let process ic =
     let md = Omd.of_channel ic in
-    output_string output (Omd.to_html ~printer md);
+    output_string output (Omd.to_html md);
     flush output
   in
   if !input = [] then
