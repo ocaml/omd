78a383c29f0ea6a786d8f4440d2a43d1ab372929
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2020-06-20 17:44:28 +0200

    Refresh omd driver

--- bin/main.ml
+++ bin/main.ml
@@ -1,51 +1,51 @@
-type format =
-  | Sexp
-  | Html
-  | Markdown
-  | Text
+let with_open_in fn f =
+  let ic = open_in fn in
+  match f ic with
+  | r -> close_in ic; r
+  | exception e -> close_in_noerr ic; raise e
 
-let fmt = ref Html
+let with_open_out fn f =
+  let oc = open_out fn in
+  match f oc with
+  | r -> close_out oc; r
+  | exception e -> close_out_noerr oc; raise e
 
-let convert ast =
-  match !fmt with
-  | Html -> Omd.to_html ast
-  | Sexp -> Omd.to_sexp ast
-  | Markdown | Text -> assert false
+let process ic oc =
+  let md = Omd.of_channel ic in
+  output_string oc (Omd.to_html md)
+
+let input = ref []
+
+let output = ref ""
+
+let spec =
+  [
+    "-o", Arg.Set_string output,
+    " file.html Specify the output file (default is stdout).";
+
+    "--", Rest(fun s -> input := s :: !input),
+    " Consider all remaining arguments as input file names.";
+
+    "-version", Unit(fun () -> print_endline "This is version VERSION."; exit 0),
+    " Print version.";
+  ]
 
 let main () =
-  let input = ref [] in
-  let output = ref "" in
-  let spec =
-    let open Arg in
-    [
-      "-o", Set_string output,
-      " file.html Specify the output file (default is stdout).";
-
-      "-sexp", Unit (fun () -> fmt := Sexp), " sexp";
-
-      "--", Rest(fun s -> input := s :: !input),
-      " Consider all remaining arguments as input file names.";
-
-      "-version", Unit(fun () -> print_endline "This is version VERSION."; exit 0),
-      " Print version.";
-    ]
+  Arg.parse (Arg.align spec)
+    (fun s -> input := s :: !input)
+    "omd [options] [inputfile1 .. inputfileN] [options]";
+  let with_output f =
+    if !output = "" then
+      f stdout
+    else
+      with_open_out !output f
   in
-  Arg.parse (Arg.align spec) (fun s -> input := s :: !input) "omd [options] [inputfile1 .. inputfileN] [options]";
-  let output = if !output = "" then stdout else open_out_bin !output in
-  let process ic =
-    let md = Omd.of_channel ic in
-    output_string output (convert md);
-    flush output
-  in
-  if !input = [] then
-    process stdin
-  else
-    List.iter (fun filename ->
-        let ic = open_in filename in
-        match process ic with
-        | () -> close_in ic
-        | exception e -> close_in_noerr ic; raise e
-      ) !input
+  with_output @@ fun oc ->
+  if !input = [] then process stdin oc
+  else begin
+    let f filename = with_open_in filename @@ fun ic -> process ic oc in
+    List.iter f !input
+  end
 
 let () =
   try
