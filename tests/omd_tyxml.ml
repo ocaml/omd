3d654e57bdacc156a0ffaf3d68624158c5493ed9
Shon Feder <shon.feder@gmail.com>
2020-07-05 09:38:25 -0400

    Add more denormalizations

--- tests/omd_tyxml.ml
+++ tests/omd_tyxml.ml
@@ -1,14 +1,19 @@
 (* Integration tests for conversion from [Omd.block] to [_ Tyxml.elt] *)
 
+let close_angle s =
+  let re = Str.regexp "\"/>" in
+  Str.global_replace re "\" />" s
+
 let br_tag s =
   let re = Str.regexp_string "<br/>" in
   Str.global_replace re "<br />\n" s
+
 
 (* The reference spec.txt has some idiosyncratic tagging and line breaks.
    So we deform the Tyxml a bit to match. Since it's all programmatic and
    clearly understood, it doesn't undermine the validity of our tests. *)
 let denormalize_html s =
-  s |> br_tag
+  s |> close_angle |> br_tag
 
 let tyxml_elt_to_string t =
   Format.asprintf "%a" Tyxml.Html.(pp_elt ~indent:false ()) t
@@ -16,7 +21,8 @@ let tyxml_elt_to_string t =
 let html_of_omd s =
   let html =
     s
-    |> List.map (fun b -> b |> Omd_tyxml.of_block |>  tyxml_elt_to_string)
+    |> List.map (fun b -> b |> Omd_tyxml.of_block |> Option.map tyxml_elt_to_string)
+    |> List.filter_map Fun.id (* FIXME *)
     |> String.concat "\n"
   in
   html ^ "\n"
