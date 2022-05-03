7259e325b4e502530f701cbded78e2ab0dbea187
Corentin Leruth <corentin.leruth@gmail.com>
2022-05-03 19:23:51 +0200

    add constructors for values of type `Omd.doc`
    Fixes: #252

--- src/omd.ml
+++ src/omd.ml
@@ -14,6 +14,41 @@ let parse_inlines (md, defs) =
   in
   List.map (Mapper.map (parse_inline defs)) md
 
+let txt ?(attrs = []) s = Text (attrs, s)
+
+let em ?(attrs = []) il = Emph (attrs, il)
+
+let strong ?(attrs = []) il = Strong (attrs, il)
+
+let code ?(attrs = []) s = Code (attrs, s)
+
+let hard_break attrs = Hard_break attrs
+
+let soft_break attrs = Soft_break attrs
+
+let link ?(attrs = []) li = Link (attrs, li)
+
+let img ?(attrs = []) li = Image (attrs, li)
+
+let html ?(attrs = []) s = Html (attrs, s)
+
+let pg ?(attrs = []) il = Paragraph (attrs, il)
+
+let ul ?(attrs = []) ?(spacing = Loose) l = List (attrs, Bullet '-', spacing, l)
+
+let ol ?(attrs = []) ?(spacing = Loose) l =
+  List (attrs, Ordered (1, '.'), spacing, l)
+
+let blq ?(attrs = []) blocks = Blockquote (attrs, blocks)
+
+let hr attrs = Thematic_break attrs
+
+let code_block ?(attrs = []) ~label s = Code_block (attrs, label, s)
+
+let html_block ?(attrs = []) s = Html_block (attrs, s)
+
+let def_list ?(attrs = []) l = Definition_list (attrs, l)
+
 let of_channel ic = parse_inlines (Pre.of_channel ic)
 let of_string s = parse_inlines (Pre.of_string s)
 let to_html doc = Html.to_string (Html.of_doc doc)
--- src/omd.mli
+++ src/omd.mli
@@ -46,6 +46,48 @@ type 'attr block =
 type doc = attributes block list
 (** A markdown document *)
 
+val txt : ?attrs:attributes -> string -> attributes inline
+
+val em : ?attrs:attributes -> attributes inline -> attributes inline
+
+val strong : ?attrs:attributes -> attributes inline -> attributes inline
+
+val code : ?attrs:attributes -> string -> attributes inline
+
+val hard_break : attributes -> attributes inline
+
+val soft_break : attributes -> attributes inline
+
+val link : ?attrs:attributes -> attributes link -> attributes inline
+
+val img : ?attrs:attributes -> attributes link -> attributes inline
+
+val html : ?attrs:attributes -> string -> attributes inline
+
+val pg : ?attrs:attributes -> attributes inline -> attributes block
+
+val ul :
+     ?attrs:attributes
+  -> ?spacing:list_spacing
+  -> attributes block list list
+  -> attributes block
+
+val ol :
+     ?attrs:attributes
+  -> ?spacing:list_spacing
+  -> attributes block list list
+  -> attributes block
+
+val blq : ?attrs:attributes -> attributes block list -> attributes block
+
+val hr : attributes -> attributes block
+
+val code_block : ?attrs:attributes -> label:string -> string -> attributes block
+
+val html_block : ?attrs:attributes -> string -> attributes block
+
+val def_list : ?attrs:attributes -> attributes def_elt list -> attributes block
+
 val of_channel : in_channel -> doc
 val of_string : string -> doc
 val to_html : doc -> string
